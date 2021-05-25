5b826c9836e0cf38955c3a0bd404a9260dafe96e
Srishty Pandey <59336380+Srishty757@users.noreply.github.com>
2021-05-26 01:43:43 +0530

    Login

---
+++ FlightManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        ContextCS c = new ContextCS();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AdminLogin()
+        {
+            if (Session["u"] != null)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(AdminLogin l)
+        {
+            var x = c.AdminLogins.Where(a => a.Adminname == l.Adminname && a.Password == l.Password).FirstOrDefault();
+            if(x!=null)
+            {
+                Session["u"] = l.Adminname;
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.m = "Wrong id or Password";
+            }
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+    }
+}
